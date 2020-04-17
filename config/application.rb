1d34ca829fba332a545e6143df56171b37590399
Redvan <redvanisation@gmail.com>
2020-04-17 17:41:59 -0400

    Update Cors

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module FreeRunApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', 'localhost:3001', 'objective-payne-c9a208.netlify.app'
+        origins '*',
         resource '*', 
         :headers => :any, 
         :methods => [:get, :post, :put, :patch, :delete, :options, :head],
