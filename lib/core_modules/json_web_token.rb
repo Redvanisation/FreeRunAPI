d0e5e3c1f939f916255ba8a2b6b8e33b69e8941a
Redvan <redvanisation@gmail.com>
2020-04-18 14:58:13 -0400

    Update rails secret for Heroku

--- lib/core_modules/json_web_token.rb
+++ lib/core_modules/json_web_token.rb
@@ -1,15 +1,14 @@
 module CoreModules::JsonWebToken
   require 'jwt'
-  JWT_SECRET = Rails.application.secrets.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, JWT_SECRET)
+    JWT.encode(payload, ENV['SECRET_KEY_BASE'] )
   end
 
   def self.decode(token)
     begin
-    body = JWT.decode(token, JWT_SECRET)
+    body = JWT.decode(token, ENV['SECRET_KEY_BASE'] )
       if body then HashWithIndifferentAccess.new body[0] else return false end
     rescue JWT::ExpiredSignature, JWT::VerificationError => e
       return false
